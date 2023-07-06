da323d134d0da45bc6d07a7ad8f49c4c7c315a27
Sean Sundberg <seansund@us.ibm.com>
2023-07-05 23:48:06 -0500

    Fix gitea namespace name
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- example/stage1-gitea.tf
+++ example/stage1-gitea.tf
@@ -2,7 +2,7 @@ module "gitea_namespace" {
   source = "github.com/cloud-native-toolkit/terraform-k8s-namespace.git"
 
   cluster_config_file_path = module.cluster.config_file_path
-  name                     = var.gitea
+  name                     = "gitea"
 }
 
 module "gitea" {
